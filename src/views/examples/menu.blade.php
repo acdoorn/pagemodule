86fdba2e288f859df2e50789be59a08828cfa716
acdoorn <ac.vandoorn@student.avans.nl>
2015-01-27 16:06:58 +0100

    Fixed order in menu

--- src/views/examples/menu.blade.php
+++ src/views/examples/menu.blade.php
@@ -46,39 +46,43 @@ foreach($menu->draftmenuitems as $menuitem) {
 
 
 function loopExampleMenuitems($menuitems, $value, $parentarray, $parentid) {
-	foreach ($menuitems as $menuitem) {
-		if(!isset($parentid)) {
-			if ($menuitem->pivot->parent == null) {
-				$value .= '<li class="dd-item">
-		    					<div class="btn btn-default form-control">'.(!is_null($menuitem->alias) ? $menuitem->alias : $menuitem->title) .'</div>';
-				if(isset($parentarray[$menuitem->id])) {
-					$value .= '<ol>';
-					//for value in parentarray loopmenuitems $menuitems, $value, $parentarray, $parentid
-					$value = loopExampleMenuitems($menuitems, $value, $parentarray, $menuitem->id);
-					$value .= '</ol>';
+	for($x = 1; $x <= $menuitems->count();$x++){
+		foreach ($menuitems as $menuitem) {
+			if($x == $menuitem->pivot->order) {
+				if(!isset($parentid)) {
+					if ($menuitem->pivot->parent == null) {
+						$value .= '<li class="dd-item">
+				    					<div class="btn btn-default form-control">'.(!is_null($menuitem->alias) ? $menuitem->alias : $menuitem->title) .'</div>';
+						if(isset($parentarray[$menuitem->id])) {
+							$value .= '<ol>';
+							//for value in parentarray loopmenuitems $menuitems, $value, $parentarray, $parentid
+							$value = loopExampleMenuitems($menuitems, $value, $parentarray, $menuitem->id);
+							$value .= '</ol>';
+						}
+						else {
+
+						}
+						$value .= '</li>';
+					}
 				}
 				else {
+					if ($menuitem->pivot->parent == $parentid) {
+						$value .= '<li class="dd-item">
+				    					<div class="btn btn-default form-control">'.(!is_null($menuitem->alias) ? $menuitem->alias : $menuitem->title) .'</div>';
+						if(isset($parentarray[$menuitem->id])) {
+							$value .= '<ol>';
+							//for value in parentarray loopmenuitems $menuitems, $value, $parentarray, $parentid
+							$value = loopExampleMenuitems($menuitems, $value, $parentarray, $menuitem->id);
+							$value .= '</ol>';
+						}
+						else {
+
+						}
+						$value .= '</li>';
+					}
 
 				}
-				$value .= '</li>';
 			}
-		}
-		else {
-			if ($menuitem->pivot->parent == $parentid) {
-				$value .= '<li class="dd-item">
-		    					<div class="btn btn-default form-control">'.(!is_null($menuitem->alias) ? $menuitem->alias : $menuitem->title) .'</div>';
-				if(isset($parentarray[$menuitem->id])) {
-					$value .= '<ol>';
-					//for value in parentarray loopmenuitems $menuitems, $value, $parentarray, $parentid
-					$value = loopExampleMenuitems($menuitems, $value, $parentarray, $menuitem->id);
-					$value .= '</ol>';
-				}
-				else {
-
-				}
-				$value .= '</li>';
-			}
-
 		}
 	}
 	return $value;
